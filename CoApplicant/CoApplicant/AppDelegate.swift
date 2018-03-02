a2ae238d21e5d3a5f657d17622f0741315f26d21
Ben Pilcher <benepilcher@gmail.com>
2018-03-02 11:51:22 -0700

    Rename storyboards, include both

--- CoApplicant/CoApplicant/AppDelegate.swift
+++ CoApplicant/CoApplicant/AppDelegate.swift
@@ -49,7 +49,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     (window as! MBFingerTipWindow).strokeColor = Color.blinkerBlue
     (window as! MBFingerTipWindow).alwaysShowTouches = true
 
-    let initialController = UIStoryboard.init(name: "Main", bundle: nil).instantiateInitialViewController()
+    let initialController = UIStoryboard(name: "Long", bundle: nil).instantiateInitialViewController()
+//  let initialController = UIStoryboard(name: "Short", bundle: nil).instantiateInitialViewController()
+
     window!.rootViewController = initialController
     window!.makeKeyAndVisible()
 
